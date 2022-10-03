e1744ed1c47803dd7d10de1cee3cce3d21cc265c
Raven Van Hove <raven.vanhove@student.ap.be>
2022-10-03 14:01:56 +0200

    reorganised current components + added testfile css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import LoginForm from "./LoginForm";
-import Profile from "./Profile";
-import AddSensor from "./AddSensor";
+import LoginForm from "./components/LoginForm";
+import Profile from "./components/Profile";
+import AddSensor from "./components/AddSensor";
 import { I18n, languages } from "./util/language/I18n";
 import { LanguageSelector } from "./components/LanguageSelector";
+import "./styles.css"
 
 const App = (): JSX.Element => {
   return (
@@ -21,7 +22,6 @@ const App = (): JSX.Element => {
           alt="Placeholder logo"
           width={250}
         />
-        <p>{I18n.t("custom.test")}</p>
         <LoginForm />
       </section>
     </>
--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
+import {I18n} from "../util/language/I18n";
 
 const LoginForm = (): JSX.Element => {
   return (
   <>
     <form>
-        <label htmlFor="username">Geef hier je gebruikersnaam in: </label><br />
+        <label htmlFor="username">{I18n.t("username.text")}</label><br />
         <input type="text" name="username" id="username"/><br />
         <label htmlFor="password">Geef hier je wachtwoord in: </label><br />
         <input type="password" name="password" id="password" /><br />
