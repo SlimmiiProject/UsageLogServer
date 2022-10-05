7782e52bd12d97ba9ce60b6309ce068c0f9bf15a
Kasper <kasperbosteels@hotmail.com>
2022-10-05 19:14:14 +0200

    added new columns to user

--- server/src/data/entities/User.ts
+++ server/src/data/entities/User.ts
@@ -1,19 +1,58 @@
-import { BaseEntity, Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { Device } from "./Device";
 
 @Entity()
-export class UserAcount extends BaseEntity  {
-
-
-  @PrimaryColumn("varchar", { unique: true, nullable: false, length: 25 })
+export class UserAcount extends BaseEntity {
+  @PrimaryColumn("varchar", {
+    unique: true,
+    nullable: false,
+    length: 25,
+    name: "userid",
+  })
   userId!: string;
 
-  @Column("varchar", { nullable: false, unique: false })
+  @Column("varchar", {
+    nullable: false,
+    unique: false,
+    name: "firstname",
+    length: 30,
+  })
+  firstname: string;
+
+  @Column("varchar", {
+    nullable: false,
+    unique: false,
+    name: "lastname",
+    length: 30,
+  })
+  lastname: string;
+
+  @Column("varchar", { nullable: false, unique: false, name: "password" })
   password!: string;
 
-  @Column("varchar", { nullable: false, unique: true, length: 60 })
+  @Column("varchar", {
+    nullable: false,
+    unique: true,
+    length: 50,
+    name: "email",
+  })
   email!: string;
 
   @Column("varchar", { nullable: false, length: 11, unique: true })
   phone!: string;
 
+  @OneToMany(() => Device, (device) => device.deviceId, {
+    nullable: true,
+    cascade: true,
+  })
+  @JoinColumn({ name: "device" })
+  device!: Device;
 }
