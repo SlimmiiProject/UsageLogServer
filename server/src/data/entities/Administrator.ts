13306f9ed99472dcac7cb16a4a7c2435f045345b
Kasper <kasperbosteels@hotmail.com>
2022-10-05 19:15:49 +0200

    added name to user relation

--- server/src/data/entities/Administrator.ts
+++ server/src/data/entities/Administrator.ts
@@ -1,13 +1,18 @@
-import { BaseEntity, Entity, JoinColumn, JoinTable, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BaseEntity,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { UserAcount } from "./User";
 
 @Entity()
 export class Administrator extends BaseEntity {
-  
   @PrimaryGeneratedColumn()
   adminId!: number;
 
-  @OneToOne(() => UserAcount, { nullable: false })
-  @JoinColumn()
+  @OneToOne(() => UserAcount, { nullable: false, cascade: false })
+  @JoinColumn({ name: "user" })
   user!: UserAcount;
 }
