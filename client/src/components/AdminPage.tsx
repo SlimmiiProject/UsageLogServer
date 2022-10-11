1d9b8b9fb05d2bb125499d5a17dae82e37df907b
Raven Van Hove <raven.vanhove@student.ap.be>
2022-10-11 15:50:44 +0200

    fixed bug

---
+++ client/src/components/AdminPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const AdminPage = (): JSX.Element => {
+  return (
+    <>
+      <div className="box">
+        <h1>Admin Panel</h1>
+      </div>
+    </>
+  );
+};
--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,37 +1,18 @@
-import  Drawer  from "./DrawerComponent";
+import Drawer from "./DrawerComponent";
 import React, { useEffect, useState } from "react";
 import { I18n, languages } from "../util/language/I18n";
 import AddSensor from "./AddSensor";
 import { LanguageSelector } from "./LanguageSelector";
-import ProfileButton from "./ProfileButton";
-import Profile from "./ProfileButton";
 import SignIn from "./SignIn";
-
 
 const HomePage = (): JSX.Element => {
   return (
     <>
-    <section className="titleFlex">
-        <h1>Slimmii CommV</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, vel!
-        </p>
-        <ProfileButton />
-    </section>
-    <AddSensor />
-    <LanguageSelector languages={languages} />
-    <section>
-      <img
-        src="./images/slimmii_500x500.png"
-        alt="Placeholder logo"
-        width={250}
-      />
-      <p>{I18n.t("custom.test")}</p>
-      <SignIn/>
-    </section>
-  </>
+      <section className="titleFlex">
+        <SignIn />
+      </section>
+    </>
   );
 };
-
 
 export default HomePage;
