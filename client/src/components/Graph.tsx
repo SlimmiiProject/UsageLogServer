7f7fb71e0c2402c15a64a2123a56b9904778ff69
Raven Van Hove <raven.vanhove@student.ap.be>
2022-10-18 21:26:41 +0200

    updated layout graphs

--- client/src/components/Graph.tsx
+++ client/src/components/Graph.tsx
@@ -26,26 +26,28 @@ export default function App(props: MeerdereData) {
   return (
     <>
       <h3>{props.titel}</h3>
-      <BarChart
-        width={800}
-        height={500}
-        data={props.data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <ReferenceLine y={0} stroke="#000" />
-        <Bar dataKey="dag" fill="#82ca9d" />
-        <Bar dataKey="nacht" fill="#8884d8" />
-      </BarChart>
+      <div className="flex">
+        <BarChart
+          width={900}
+          height={500}
+          data={props.data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <ReferenceLine y={0} stroke="#000" />
+          <Bar dataKey="dag" fill="#ffbc40" />
+          <Bar dataKey="nacht" fill="#00008b" />
+        </BarChart>
+      </div>
     </>
   );
 }
