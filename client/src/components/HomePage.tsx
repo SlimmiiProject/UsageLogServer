6bdf535bff58fe21b77123b3a838cb8ea4185cf4
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-10-18 15:13:04 +0200

    imports

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,8 +1,3 @@
-import Drawer from "./DrawerComponent";
-import React, { useEffect, useState } from "react";
-import { I18n, languages } from "../util/language/I18n";
-import AddSensor from "./AddSensor";
-import { LanguageSelector } from "./LanguageSelector";
 import SignIn from "./SignIn";
 
 const HomePage = (): JSX.Element => {
