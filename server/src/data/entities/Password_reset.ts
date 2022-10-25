2cfeb7f0b65dca678f47716761294124bb8e0bec
Kasper Bosteels <kasperbosteels@hotmail.com>
2022-10-25 18:32:42 +0200

    added password reset table

---
+++ server/src/data/entities/Password_reset.ts
@@ -0,0 +1,23 @@
+import {
+  BaseEntity,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { UserAccount } from "./UserAccount";
+
+@Entity()
+export class Password_Reset extends BaseEntity {
+  @PrimaryColumn("varchar", { name: "token", unique: true, nullable: false })
+  Token: string;
+
+  @OneToOne(() => UserAccount, { nullable: false, cascade: false })
+  @JoinColumn({ name: "user" })
+  user: UserAccount;
+
+  @CreateDateColumn({ name: "created_at" })
+  created_at: Date;
+}
