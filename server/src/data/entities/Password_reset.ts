424073410d6d8cfd9bd7c274a71ae090ca1ea00b
Kasper Bosteels <kasperbosteels@hotmail.com>
2022-10-27 23:21:07 +0200

    password validation

--- server/src/data/entities/Password_reset.ts
+++ server/src/data/entities/Password_reset.ts
@@ -1,3 +1,4 @@
+import { IsDefined } from "class-validator";
 import {
   BaseEntity,
   CreateDateColumn,
@@ -11,10 +12,12 @@ import { UserAccount } from "./UserAccount";
 @Entity()
 export class Password_Reset extends BaseEntity {
   @PrimaryColumn("varchar", { name: "token", unique: true, nullable: false })
+  @IsDefined()
   token: string;
 
   @OneToOne(() => UserAccount, { nullable: false, cascade: false })
   @JoinColumn({ name: "user" })
+  @IsDefined()
   user: UserAccount;
 
   @CreateDateColumn({ name: "created_at" })
