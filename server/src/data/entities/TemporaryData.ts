6412f2eb9128c413d40e9df861623a6153a54978
Kasper Bosteels <kasperbosteels@hotmail.com>
2022-10-27 23:20:56 +0200

    temp data validation

--- server/src/data/entities/TemporaryData.ts
+++ server/src/data/entities/TemporaryData.ts
@@ -1,27 +1,32 @@
+import { IsDefined, IsInt, IsOptional } from "class-validator";
 import {
   Entity,
   BaseEntity,
   Column,
   CreateDateColumn,
-  PrimaryColumn,
   PrimaryGeneratedColumn,
   ManyToOne,
 } from "typeorm";
 import { Device } from "./Device";
 @Entity()
 export class TemporaryData extends BaseEntity {
-
   @PrimaryGeneratedColumn({ name: "index" })
   index!: number;
 
   @ManyToOne(() => Device, (device) => device.device_index, { nullable: false })
+  @IsDefined()
   device: Device;
 
   @Column("numeric", { name: "day", nullable: true, unique: false })
+  @IsOptional()
+  @IsInt()
   Day: number;
 
   @Column("numeric", { name: "night", nullable: true, unique: false })
+  @IsOptional()
+  @IsInt()
   Night: number;
+
   @CreateDateColumn({ name: "created_at" })
   created_at: Date;
 }
