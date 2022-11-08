067bee796fb96dad89e0ef4e7c073aef2870346b
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-11-08 12:56:27 +0100

    Reading from directory for languages

--- server/src/data/DataController.ts
+++ server/src/data/DataController.ts
@@ -1,16 +1,19 @@
 import { Translations } from "./entities/Translations";
+import fs from "fs";
+import path from "path";
 
 export class DataController {
 
     public static async getTranslationJson(): Promise<Object> {
         let translationSetup: any = {};
 
-        const languages: Translations[] = await Translations.find();
+        const mainFile: { [key: string]: string } = JSON.parse(fs.readFileSync(__dirname +"/languages/_main.json", { encoding: "utf8" }));
 
-        languages.forEach(language => {
-            translationSetup[language.lang_key] = {
-                nativeName: language.nativeName,
-                translation: JSON.parse(language.translations)
+        Object.entries(mainFile).forEach((pair) => {
+            console.log(pair[0])
+            translationSetup[pair[0]] = {
+                nativeName: pair[1],
+                translation: JSON.parse(fs.readFileSync(path.join( __dirname,"/languages/", pair[0] + ".json"), {encoding: "utf-8"}))
             }
         });
 
