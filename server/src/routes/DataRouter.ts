e5d17123e1e64d1cacd49235006f8f25143c3ec8
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-11-08 12:26:45 +0100

    More work on data

--- server/src/routes/DataRouter.ts
+++ server/src/routes/DataRouter.ts
@@ -19,10 +19,43 @@ router.post("/meter-entry", (req: Request, res: Response) => {
     // TODO Receives data from local python program with JSON data about the meter entry
 });
 
-// router.get("/week-data", onlyAcceptJSON, async (req: Request, res: Response) => {
-//     const userData: User = SessionManager.getSessionData(req).user;
-//     const data: DeviceSpecificData[] = await DataProcessor.GetData(userData.id)
-// });
+
+type Period = "Day" | "Week" | "Month";
+
+type DataParams = { [key: string]: string } & {
+    period: Period,
+    beginDate: number;
+};
+
+const dayInMS = 86_400_000;
+
+router.get("/data", onlyAcceptJSON, async (req: Request, res: Response) => {
+    const userData: User = SessionManager.getSessionData(req).user;
+    const data: DeviceSpecificData[] = await DataProcessor.GetData(userData.id);
+    const params: DataParams = req.params as DataParams;
+
+    let begin: Date = new Date(params.beginDate);
+    let endDate: Date;
+
+    // Calculate End Days
+    switch (params.period) {
+        case "Day":
+            endDate = new Date(begin.setDate(begin.getDate() + 1));
+            break;
+        case "Week":
+            endDate = new Date(begin.setDate(begin.getDate() + (7 - begin.getDay())));
+            break;
+        case "Month":
+            endDate = new Date(begin.getFullYear(), begin.getMonth() + 1, 0);
+            break;
+    }
+
+    // TODO Wait for TypeORM changes to get data between begin and end
+});
+
+const getDayOfMonth = (year: number, month: number, day: number) => {
+    return new Date(year, month, day);
+}
 
 
 module.exports = router;
