ebabb494c88231996729f64b2ef9b880f54c7633
Kasper B <kasperbosteels@hotmail.com>
2022-11-12 19:18:59 +0100

    added more passing emails

--- server/src/tests/RegexValidator.test.ts
+++ server/src/tests/RegexValidator.test.ts
@@ -11,9 +11,32 @@ const values = [
 const badValues = [
     "QnJaswva2Vu",
     "VWdoIasFVuaXRz"
-] 
+]
 
+const passingEmails = [
+    "personsName@coldmail.com",
+    "abc@abd.Museums",
+    "a@E.C",
+    "A".repeat(64) + "@" + "B".repeat(175) + ".com",
+    'Abc\\@def@example.com',
+    "Fred\\Bloggs@example.com",
+    "Joe.\\Blow@example.com",
+    'Abc@def"@example.com',
+    'Fred Bloggs"@example.com',
+    "customer/department=shipping@example.com",
+]
+const failingEmails = [
+    "personsnames.ColdMail.com",
+    "abc@@abd.Museums",
+    "abcabd.Museums"
+]
 test("Base64 Regex works correctly", () => {
     values.forEach((b) => expect(RegExpVal.validate(b, RegExpVal.base64Encoded)).toBeTruthy());
     badValues.forEach((b) => expect(RegExpVal.validate(b, RegExpVal.base64Encoded)).toBeFalsy());
 });
+
+test("email Validators work correctly", () => {
+    passingEmails.forEach((passing) => expect(RegExpVal.validate(passing, RegExpVal.emailValidator)).toBeTruthy());
+    //for some reason toBeFalsly fails when "()" is added to the end
+    failingEmails.forEach((failing) => expect(RegExpVal.validate(failing, RegExpVal.emailValidator)).toBeFalsy);
+})
