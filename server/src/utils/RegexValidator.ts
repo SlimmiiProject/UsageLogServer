fd406ca5adb12a9324cf9ee0e1af24e815132f24
Kasper B <kasperbosteels@hotmail.com>
2022-11-20 17:50:08 +0100

    improved phone regex

--- server/src/utils/RegexValidator.ts
+++ server/src/utils/RegexValidator.ts
@@ -1,7 +1,7 @@
 export class RegExpVal {
 
     // TODO Fill in RegEx
-    public static readonly phoneValidator = new RegExp('^\+324[0-9]{8}$');
+    public static readonly phoneValidator = new RegExp('^\+324{1}\d{8}$');
     //pls don't delete yet public static readonly emailValidator = new RegExp('^([a-zA-Z0-9.!#$%&’*+/\\=?^_`{|}~-]{1,63})+([@]{1})([a-zA-Z0-9-]{1,190})+(?:\.([a-zA-Z0-9-]{1})+)*$');
     public static readonly emailValidator = new RegExp('^[\w!#$%&,*+\/\\=?^`{|}.~-]{1,63}@\w{1,190}(?:.(\w{1})+)*$');
     public static readonly alphabetValidator = new RegExp('^([A-Za-zÀ-ÖØ-öø-ÿ])+$');
