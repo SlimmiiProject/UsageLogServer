385a3b086be0131c9c4c3b2e14f03869d95f1185
Elwyn Van der Borght <38124619+ElwynVdb@users.noreply.github.com>
2022-11-22 23:38:48 +0100

    made graph transparent (#35)
    Co-authored-by: Kasper B <kasperbosteels@hotmail.com>

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ export default function App({ data: { devices } }: { data: { devices: IDevice[] 
     <div className="flex">
       {devices.map((meter) => {
         return (
-          <div className="graph">
+          <div className="graph" style={{backgroundColor:"rgba(0, 0, 0, 0.0)"}}>
             <Graph data={meter.data} titel={meter.nameDevice} colorDay={meter.colorDay} colorNight={meter.colorNight} />
           </div>
         );
