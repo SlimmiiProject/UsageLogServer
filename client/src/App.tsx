f6125e29573dd13dbc6de2190eb07abc6f6ee19b
Elwyn Van der Borght <38124619+ElwynVdb@users.noreply.github.com>
2022-11-22 23:54:57 +0100

    Made darkmode consistent & Context shift

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import Profile from "./components/users/Profile";
 import Register from "./components/users/Register";
 import Devices from "./components/users/Devices";
@@ -7,22 +11,13 @@ import Logout from "./components/users/Logout";
 import Drawer from "./components/DrawerComponent";
 import Contact from "./components/Contact";
 import PageNotFound from "./components/404";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import DashboardComp from "./components/dashboard/Dashboard";
 import { AdminPage } from "./components/admin/AdminPage";
 import LoginPage from "./components/users/LoginPage";
 import { I18n } from "./util/language/I18n";
 import EditProfile from "./components/users/EditProfile";
 import { Routes, Route, Navigate } from "react-router-dom";
-import {
-  createTheme,
-  CssBaseline,
-  ThemeProvider,
-  useMediaQuery,
-} from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 export interface ITestData {
   devices: IDevice[];
@@ -59,16 +54,13 @@ export const userContext = React.createContext<IuserContext>({
 });
 
 export const setUserContext = React.createContext<ISetUserContext>({
-  setLoggedIn: (loggedIn: boolean) => {},
-  setAdmin: (isAdmin: boolean) => {},
+  setLoggedIn: (loggedIn: boolean) => { },
+  setAdmin: (isAdmin: boolean) => { },
 });
-// get the current path to use for the correct links
+
+// Get the current path to use for the correct links
 export const getCurrentPath = (location: any) => {
-  if (location.pathname[location.pathname.length - 1] === "/")
-    location.pathname = location.pathname.substring(
-      0,
-      location.pathname.length - 1
-    );
+  if (location.pathname[location.pathname.length - 1] === "/") location.pathname = location.pathname.substring(0, location.pathname.length - 1);
   return location.pathname;
 };
 
@@ -79,7 +71,7 @@ export const getPath = (path: string) => {
 const App = (): JSX.Element => {
   const [loggedIn, setLoggedIn] = useState<boolean>(true);
   const [isAdmin, setIsAdmin] = useState<boolean>(true);
-  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [darkMode, setDarkMode] = useState<boolean>(JSON.parse(localStorage.getItem("darkMode")!) || false);
   const [userId, setUserId] = useState<string>("");
 
   const lang = I18n.currentLanguage;
@@ -95,54 +87,54 @@ const App = (): JSX.Element => {
     },
   });
   const handleDarkMode = () => {
-    setDarkMode((prevDarkMode) => !prevDarkMode);
+    setDarkMode((prevDarkMode) => {
+      const afterToggle = !prevDarkMode;
+      localStorage.setItem("darkMode", JSON.stringify(afterToggle));
+      return afterToggle;
+    });
+
   };
   return (
     <>
+
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-        <userContext.Provider
-          value={{
-            loggedIn: loggedIn,
-            isAdmin: isAdmin,
-            userId: userId,
-          }}
-        >
-          <Drawer lang={lang} onDarkmode={handleDarkMode} mode={darkMode} />
-          <Routes>
-            <Route path="/" element={<Navigate to={`/${lang}/`} />} />
-            <Route
-              path="/dashboard"
-              element={<Navigate to={`/${lang}/dashboard`} />}
-            />
-            <Route path="/:lang">
-              <Route index element={<LoginPage />} />
+        <userContext.Provider value={{ loggedIn: loggedIn, isAdmin: isAdmin, userId: userId }} >
+          <setUserContext.Provider value={{ setLoggedIn: setLoggedIn, setAdmin: setIsAdmin }}>
+            <Drawer lang={lang} onDarkmode={handleDarkMode} mode={darkMode} />
+            <Routes>
+              <Route path="/" element={<Navigate to={`/${lang}/`} />} />
               <Route
-                path="dashboard"
-                element={<DashboardComp data={combineddata} />}
+                path="/dashboard"
+                element={<Navigate to={`/${lang}/dashboard`} />}
               />
-              {/* <setUserContext.Provider
-                value={{
-                  setLoggedIn: setLoggedIn,
-                  setAdmin: setIsAdmin,
-                }}
-              > */}
-              <Route path="login" element={<LoginPage />} />
-              <Route path="register" element={<Register />} />
-              <Route path="logout" element={<Logout />} />
-              {/* </setUserContext.Provider> */}
-              <Route path="profile" element={<Profile />} />
-              <Route path="profile/edit-profile" element={<EditProfile />} />
-              <Route path="devices" element={<Devices />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="admin" element={<AdminPage />}>
-                <Route path="allusers" element={<AdminPage />} />
-                <Route path="alldevices" element={<AdminPage />} />
-                <Route path="logfile" element={<AdminPage />} />
+
+              <Route path="/:lang">
+                <Route index element={<LoginPage />} />
+                <Route
+                  path="dashboard"
+                  element={<DashboardComp data={combineddata} />}
+                />
+
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<Register />} />
+                <Route path="logout" element={<Logout />} />
+
+                <Route path="profile" element={<Profile />} />
+                <Route path="profile/edit-profile" element={<EditProfile />} />
+                <Route path="devices" element={<Devices />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="admin" element={<AdminPage />}>
+                  <Route path="allusers" element={<AdminPage />} />
+                  <Route path="alldevices" element={<AdminPage />} />
+                  <Route path="logfile" element={<AdminPage />} />
+                </Route>
+
               </Route>
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </setUserContext.Provider>
         </userContext.Provider>
       </ThemeProvider>
     </>
