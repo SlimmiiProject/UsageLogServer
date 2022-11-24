17802d51ef53202745bc788f77361c3a83cdc58e
Kasper B <kasperbosteels@hotmail.com>
2022-11-24 19:57:57 +0100

    added request to logfile and state

--- client/src/components/admin/LogFile.tsx
+++ client/src/components/admin/LogFile.tsx
@@ -1,22 +1,34 @@
 import { I18n } from "../../util/language/I18n";
 import {List,ListItem,ListItemText,Typography} from "@mui/material";
-import {Logfile} from "../../../../server/src/data/entities/Logfile";
-export const LogFile = (): JSX.Element => {
-  const files:Logfile[] = [];
-  return (
+import { AdminUtil } from "../../util/AdminUtil";
+import { useState } from "react";
+
+export const LogFile = ():JSX.Element => {
+const [files,setFiles] = useState<any[]>([]);
+const [isloading,setisloading]=useState<boolean>(false);
+const populateFiles = ()=>{
+  AdminUtil.getLogs().then((result)=>{
+    console.log(result)
+  setFiles(result)
+  setisloading(!isloading);
+  } )
+}  
+populateFiles();
+return (
     <>
       <div className="flexbox">
         <h2>{I18n.t("logger.title")}</h2>
         <List>
           <ListItem alignItems="flex-start">
-            {files.map((file)=>(
+
+            {isloading ? ( files.map((file)=>(
                 <ListItemText primary={file.date.toString()} secondary={
                   <>
                   <Typography sx={{display:"inline"}} component="span" color="text.primary"><>{I18n.t("logger.acount_id")}: {file.account_id}</></Typography>
                   {`${I18n.t("logger.Description")}: ${file.description} ${I18n.t("logger.address")}: ${file.ipaddress}`}
                   </>
                 }/>
-            ))}
+            ))) : (<><h2>LOADING...</h2></>)}
           </ListItem>
         </List>
       </div>
