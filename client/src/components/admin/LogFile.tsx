b8daf74ed9ded66e7e665640bca10c374382a010
Kasper B <kasperbosteels@hotmail.com>
2022-11-24 18:32:49 +0100

    Logfile files mapping added

--- client/src/components/admin/LogFile.tsx
+++ client/src/components/admin/LogFile.tsx
@@ -1,10 +1,23 @@
 import { I18n } from "../../util/language/I18n";
-
+import {List,ListItem,ListItemText,Typography} from "@mui/material";
 export const LogFile = (): JSX.Element => {
+  const files:Logfile[] = [];
   return (
     <>
       <div className="flexbox">
         <h2>{I18n.t("logger.title")}</h2>
+        <List>
+          <ListItem alignItems="flex-start">
+            {files.map((file)=>(
+                <ListItemText primary={file.date.toString()} secondary={
+                  <>
+                  <Typography sx={{display:"inline"}} component="span" color="text.primary"><>{I18n.t("logger.acount_id")}: {file.account_id}</></Typography>
+                  {`${I18n.t("logger.Description")}: ${file.description} ${I18n.t("logger.address")}: ${file.ipaddress}`}
+                  </>
+                }/>
+            ))}
+          </ListItem>
+        </List>
       </div>
     </>
   );
