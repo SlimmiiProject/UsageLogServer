5e9e3ebb53eb1ee4853912165323d414e8217b27
Kasper B <kasperbosteels@hotmail.com>
2022-11-25 19:05:46 +0100

    added users loading

--- client/src/components/admin/AllUsers.tsx
+++ client/src/components/admin/AllUsers.tsx
@@ -1,13 +1,28 @@
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { AdminUtil, userData } from "../../util/AdminUtil";
 import { I18n } from "../../util/language/I18n";
 
 export const AllUsers = (): JSX.Element => {
+  const [users,setusers] = useState<userData[]>([]);
+  const [isloading,setisloading] = useState<boolean>(true);
+
+  useEffect(()=>{
+    const controller = new AbortController();
+    setisloading(true);
+    AdminUtil.getUsers(controller).then((result)=>{
+      setusers(result);
+      setisloading(false);
+    });
+    return ()=>controller.abort();
+  },[])
   return (
     <>
-      <div className="flexbox">
+      <Box className="flexbox">
         <AccountCircleRoundedIcon />
         <h2>{I18n.t("allUsers.List")}</h2>
-      </div>
+      </Box>
     </>
   );
 };
