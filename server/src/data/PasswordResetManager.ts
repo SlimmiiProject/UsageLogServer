5df7fd7f3d84e700beb0a49abe26019abc5a9f0e
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-11-29 16:50:58 +0100

    Base Functions Reset

--- server/src/data/PasswordResetManager.ts
+++ server/src/data/PasswordResetManager.ts
@@ -4,21 +4,27 @@ import { PasswordReset } from "./entities/PasswordReset";
 export class PasswordResetManager {
 
     private _token: string;
-    private _resetEntry:PasswordReset;
+    private _newPassword:string;
+    private _resetEntry: PasswordReset;
 
-    constructor(token:string) {
+    constructor(token: string, new_password:string) {
         this._token = token;
+        this._newPassword = new_password;
     }
 
     private setup = async () => {
         this._resetEntry = await DataProcessor.GetPasswordReset(this._token);
     }
 
-    private isValid = () => {
-
+    private get isValid() {
+        return (Date.now() - this._resetEntry.created_at.getMilliseconds()) > (30 * 60 * 1000); // 30 Minutes
     }
 
     public handle = async () => {
         await this.setup();
+
+        if (this.isValid) {
+            
+        }
     }
 }
