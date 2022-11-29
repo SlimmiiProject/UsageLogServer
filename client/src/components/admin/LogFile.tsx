ea4c48604ba9828c55ce460898aed42957da334e
Kasper B <kasperbosteels@hotmail.com>
2022-11-29 16:50:52 +0100

    AAAAAAHHHHH werk ni

--- client/src/components/admin/LogFile.tsx
+++ client/src/components/admin/LogFile.tsx
@@ -1,7 +1,7 @@
 import { I18n } from "../../util/language/I18n";
-import { List, ListItem, ListItemText, Typography, Box } from "@mui/material";
 import { AdminUtil, LogData } from "../../util/AdminUtil";
 import { useEffect, useState } from "react";
+import { Box, List, ListItem, Typography, Chip, TableContainer, Table, TableBody, TableCell, TableHead, TableRow, Paper } from "@mui/material";
 
 export const LogFile = (): JSX.Element => {
   const [files, setFiles] = useState<LogData[]>([]);
@@ -21,23 +21,34 @@ export const LogFile = (): JSX.Element => {
 
   return (
     <>
-      <Box className="flexbox" style={{margin:"auto", justifyContent:"center",flexDirection:"column", display:"flex", alignItems:"center" , borderWidth:3,borderColor:"grey",borderRadius:15, width:500,height:"fit-content", backgroundColor:"rgba(0,0,0,0.0)"}}>
-        <h2>{I18n.t("logger.title")}</h2>
-        <List>
-          {!isloading ? (files.map((file)=>(
-            <ListItem alignItems="flex-start">
-            <ListItemText primary={file.date.toString()} secondary={
-                <>
-                <div className="listItemContent">
-                  <Typography sx={{ display: "inline" }} component="span" color="text.primary"><>{I18n.t("logger.Account_id")} </></Typography>
-                  <br></br>{`${I18n.t("logger.Description")}:\n${file.description}`}<br></br>{`${I18n.t("logger.Address")}: ${file.ipaddress}`}
-                  </div>
-                </>
-              } />
-              </ListItem>
-          ))): (<><h2>LOADING...</h2></>)}
-        </List>
-      </Box>
+    <Box className="flexbox" style={{margin:"auto", justifyContent:"center",flexDirection:"column", display:"flex", alignItems:"center" , borderWidth:3,borderColor:"grey",borderRadius:15, width:500,height:"fit-content", backgroundColor:"rgba(0,0,0,0.0)"}}>
+    <h2>{I18n.t("logger.title")}</h2>
+    <TableContainer component={Paper}>
+      <Table sx={{minwidth:650}} arial-label="simple table">
+        <TableHead>
+          <TableCell>Id</TableCell>
+          <TableCell>User Account</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell>IP</TableCell>
+          <TableCell>Date</TableCell>
+        </TableHead>
+
+        <TableBody>
+          {!isloading ? (
+            files.map((file)=>(
+    <TableRow key={file.id} sx={{'&:last-child td, &:last-child th':{border:0}}}>
+        <TableCell component="th" scope="row"> {file.id}</TableCell>
+        <TableCell align="right">{file.account_id}</TableCell>
+        <TableCell align="right">{file.description}</TableCell>
+        <TableCell align="right">{file.ipaddress}</TableCell>
+        <TableCell align="right">{file.date.toDateString()}</TableCell>
+        </TableRow>
+            ))
+          ): <h2>Loading...</h2>}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Box>
     </>
   );
 };
