4ceae5f9a485870bc3832fd3d30f98c8fe865449
Kasper B <kasperbosteels@hotmail.com>
2022-12-01 18:18:52 +0100

    fixed styling

--- client/src/components/admin/LogFile.tsx
+++ client/src/components/admin/LogFile.tsx
@@ -21,13 +21,13 @@ export const LogFile = (): JSX.Element => {
 
   return (
     <>
-    <Box className="flexbox" style={{margin:"auto", justifyContent:"center",flexDirection:"column", display:"flex", alignItems:"center" , borderWidth:3,borderColor:"grey",borderRadius:15, width:500,height:"fit-content", backgroundColor:"rgba(0,0,0,0.0)"}}>
+    <Box className="flexbox" style={{margin:"auto", justifyContent:"center",flexDirection:"column", display:"flex", alignItems:"center" , borderWidth:3,borderColor:"grey",borderRadius:15, width:"70%",height:"fit-content", backgroundColor:"rgba(0,0,0,0.0)"}}>
     <h2>{I18n.t("logger.title")}</h2>
     <TableContainer component={Paper}>
       <Table sx={{minwidth:650}} arial-label="simple table">
         <TableHead>
           <TableCell>Id</TableCell>
-          <TableCell>User Account</TableCell>
+          <TableCell>Account</TableCell>
           <TableCell>Description</TableCell>
           <TableCell>IP</TableCell>
           <TableCell>Date</TableCell>
@@ -38,10 +38,10 @@ export const LogFile = (): JSX.Element => {
             files.map((file)=>(
     <TableRow key={file.id} sx={{'&:last-child td, &:last-child th':{border:0}}}>
         <TableCell component="th" scope="row"> {file.id}</TableCell>
-        <TableCell align="right">{file.account_id}</TableCell>
-        <TableCell align="right">{file.description}</TableCell>
-        <TableCell align="right">{file.ipaddress}</TableCell>
-        <TableCell align="right">{new Date(file.date).toDateString()}</TableCell>
+        <TableCell align="left">{file.account_id}</TableCell>
+        <TableCell align="left" >{file.description}</TableCell>
+        <TableCell align="left">{file.ipaddress}</TableCell>
+        <TableCell align="left">{new Date(file.date).toDateString()}</TableCell>
         </TableRow>
             ))
           ): <h2>Loading...</h2>}
