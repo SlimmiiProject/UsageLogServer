a49ae9769c1fd6b7e7d0605e2b4f1819fafa3bdc
Van Looke Bjarne (s128268) <bjarne.vanlooke@student.ap.be>
2022-12-06 14:11:17 +0100

    translations Admin

--- client/src/components/admin/AdminPageOneComponent.tsx
+++ client/src/components/admin/AdminPageOneComponent.tsx
@@ -4,15 +4,16 @@ import SettingsRemoteRoundedIcon from "@mui/icons-material/SettingsRemoteRounded
 import AddBoxRoundedIcon from "@mui/icons-material/AddBoxRounded";
 import TranslateRoundedIcon from "@mui/icons-material/TranslateRounded";
 import { NavLink } from "react-router-dom";
+import { I18n } from "../../util/language/I18n";
 
 export const AdminPageOneComponent = ({ title, icon, link }: { title: string; icon: string; link: string; }): JSX.Element => {
 
   const getIconForName = (iconName: string) => {
     switch (iconName) {
-      case "profile": return <AccountCircleRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />;
-      case "log": return <TextSnippetRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />;
-      case "device": return <SettingsRemoteRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />;
-      case "translate": return <TranslateRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />
+      case I18n.t("adminPage.profile"): return <AccountCircleRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />;
+      case I18n.t("adminPage.log"): return <TextSnippetRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />;
+      case I18n.t("adminPage.device"): return <SettingsRemoteRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />;
+      case I18n.t("adminPage.translate"): return <TranslateRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />
       default: return <AddBoxRoundedIcon style={{ fontSize: "6rem", textAlign: "center", color: "white" }} />
     }
   }
