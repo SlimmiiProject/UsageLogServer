447f96a56b2bfc0abdbfec64fbd34553c819f690
Raven Van Hove <raven.vanhove@student.ap.be>
2022-12-06 16:41:33 +0100

    dropdown

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -3,34 +3,65 @@ import { IDevice, userContext } from "../../App";
 import { IOUtil } from "../../util/IOUtil";
 import Graph from "./Graph";
 
+type timePeriod = "Day"|"Week"|"Month";
+
 export default function App() {
   const context = useContext(userContext);
   const [loading, setLoading] = useState<boolean>(false);
   const [devices, setDevices] = useState<IDevice[]>([]);
+  const [timePeriod, setTimePeriod] = useState<timePeriod>("Week");
 
   useEffect(() => {
     if (context.isLoggedIn) {
       setLoading(true);
 
       const abortController = new AbortController();
-      IOUtil.getDevicesData("Month", abortController).then(res => {
+      IOUtil.getDevicesData(timePeriod, abortController).then((res) => {
         setDevices(res);
         setLoading(false);
       });
 
       return () => abortController.abort();
     }
-  }, []);
+  }, [timePeriod]);
 
-  return (<>
-    <div className="flex">
-    
-      {!loading && devices.map((meter) => <section className="graph" style={{ backgroundColor: "rgba(0, 0, 0, 0.0)", minWidth: "700px" }}>
-        <Graph data={meter.data} titel={meter.nameDevice} colorDay={meter.colorDay} colorNight={meter.colorNight} />
-      </section>
-      )}
+  return (
+    <>
+    <select
+          className="dropdown"
+          onChange={(e) => {
+            setLoading(true);
+            setTimePeriod(e.target.value as timePeriod);
+            const abortController = new AbortController();
+            return () => abortController.abort();
+          }}
+          value={timePeriod}
+        >
+          <option value="Month">Month</option>
+          <option value="Week">Week</option>
+          <option value="Day">Day</option>
+        </select>
+      <div className="flex">
+        {!loading &&
+          devices.map((meter) => (
+            <section
+              className="graph"
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.0)",
+                minWidth: "700px",
+              }}
+            >
+              <Graph
+                data={meter.data}
+                titel={meter.nameDevice}
+                colorDay={meter.colorDay}
+                colorNight={meter.colorNight}
+              />
+            </section>
+          ))}
 
-      {!loading && devices.length === 0 && <h2>No Data</h2>}
-    </div>
-  </>);
+        {!loading && devices.length === 0 && <h2>No Data</h2>}
+      </div>
+    </>
+  );
 }
