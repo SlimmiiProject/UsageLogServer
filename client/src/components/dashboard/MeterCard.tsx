adaa36e057f1c54b1201eb3101e9a3139209edbb
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-12-13 17:27:33 +0100

    Fix

--- client/src/components/dashboard/MeterCard.tsx
+++ client/src/components/dashboard/MeterCard.tsx
@@ -1,6 +1,6 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import { I18n } from "../../util/language/I18n";
 export interface DeviceCardProps {
   deviceIndex: number;
@@ -8,25 +8,36 @@ export interface DeviceCardProps {
   friendlyName: string | undefined;
   batteryPercentage: number | undefined;
 }
-export default function BasicCard({deviceIndex,deviceId,friendlyName = I18n.t("metercard.basicCard"),batteryPercentage = -222}:DeviceCardProps) {
+export default function BasicCard({
+  deviceIndex,
+  deviceId,
+  friendlyName = I18n.t("metercard.basicCard"),
+  batteryPercentage = -222,
+}: DeviceCardProps) {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         <Typography variant="h5" component="div">
           <>
-          {I18n.t("metercard.name")}: {friendlyName ? ({friendlyName}) : <></>}
+            {I18n.t("metercard.name")}:{" "}
+            {friendlyName ? friendlyName  : <></>}
           </>
         </Typography>
-        <Typography variant="h6">{I18n.t("metercard.deviceIndex")} {deviceIndex}</Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {I18n.t("metercard.id")}{deviceId}
+        <Typography variant="h6">
+          {I18n.t("metercard.deviceIndex")} {deviceIndex}
         </Typography>
-
-        
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {I18n.t("metercard.id")}
+          {deviceId}
+        </Typography>
 
         <Typography variant="subtitle1" component="div" sx={{ opacity: "50%" }}>
           {I18n.t("metercard.battery")}
-          <Typography>{batteryPercentage === -222 ? I18n.t("metercard.bateryData") : batteryPercentage}</Typography>
+          <Typography>
+            {batteryPercentage === -222
+              ? I18n.t("metercard.bateryData")
+              : batteryPercentage}
+          </Typography>
         </Typography>
       </CardContent>
     </Card>
