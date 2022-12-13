2823892a36acca8e82707c8f9c7374826bdfece0
Kasper B <kasperbosteels@hotmail.com>
2022-12-13 15:54:11 +0100

    all devices from specific user with errors

--- client/src/components/dashboard/MeterCard.tsx
+++ client/src/components/dashboard/MeterCard.tsx
@@ -2,23 +2,31 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { I18n } from "../../util/language/I18n";
-
-export default function BasicCard() {
+export interface DeviceCardProps {
+  device_index: number;
+  deviceId: string;
+  friendlyName: string;
+  BatteryPercentage: number;
+}
+export default function BasicCard({device_index,deviceId,friendlyName,BatteryPercentage}:DeviceCardProps) {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
         <Typography variant="h5" component="div">
-          {I18n.t("metercard.name")}
+          {I18n.t("metercard.name")}:
+          {friendlyName ? (<Typography>{friendlyName}</Typography>) : <></>}
         </Typography>
 
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
           {I18n.t("metercard.id")}
+          <Typography>{deviceId}</Typography>
         </Typography>
 
         {I18n.t("metercard.chart")}
 
         <Typography variant="subtitle1" component="div" sx={{ opacity: "50%" }}>
           {I18n.t("metercard.battery")}
+          <Typography>{BatteryPercentage}</Typography>
         </Typography>
       </CardContent>
     </Card>
--- client/src/components/users/Devices.tsx
+++ client/src/components/users/Devices.tsx
@@ -1,12 +1,30 @@
+import { useState, useContext } from "react";
 import { I18n } from "../../util/language/I18n";
 import MeterCard from "../dashboard/MeterCard";
-
+import {IOUtil} from "../../util/IOUtil"
+import { userContext } from "../../App";
+import { DeviceCardProps } from "../dashboard/MeterCard";
+import { Box } from "@mui/material";
 const Devices = (): JSX.Element => {
+  const [isloading, setIsloading] = useState(true)
+  const [devices, setDevices] = useState<DeviceCardProps[]>()
+  const context  = useContext(userContext)
+  console.log(context.userAccount!.id)
+
+  IOUtil.getOwnDevice(context.userAccount!.id).then((res)=>{
+    console.log(context.userAccount!.id)
+    console.log(res)
+    setDevices(res)
+    setIsloading(false)
+  });
+  
   return (
     <>
       <h1>{I18n.t("devices.devicemanager")}</h1>
       <h2>{I18n.t("devices.sample")}</h2>
-      <MeterCard />
+      <Box>
+       {isloading && devices?.map((dev)=><MeterCard BatteryPercentage={dev.BatteryPercentage} device_index={dev.device_index} deviceId={dev.deviceId} friendlyName={dev.friendlyName}/>)}
+      </Box>
     </>
   );
 };
