778c91ed2194bfe3c862b202aeeb03373417efef
Raven Van Hove <raven.vanhove@student.ap.be>
2022-12-20 13:02:38 +0100

    update

--- client/src/util/AdminUtil.ts
+++ client/src/util/AdminUtil.ts
@@ -61,7 +61,7 @@ export class AdminUtil {
 
     /* A function that is called when a user is created. */
     public static getUsers = async (controller: AbortController, page:number): Promise<userData[]> => {
-        const toSkip = page * 20;
+        const toSkip = page * 10;
         console.log("received allusers request");
         try {
             const res = await IOUtil.INSTANCE.get("/admin/allusers/", { params: {skip: toSkip}, signal: controller.signal });
