059aafce9f51609395ae53b32946d0c08e99b817
Kasper B <kasperbosteels@hotmail.com>
2022-12-20 16:08:18 +0100

    create user dialog works

--- client/src/components/admin/AllUsers.tsx
+++ client/src/components/admin/AllUsers.tsx
@@ -15,6 +15,14 @@ import {
   IconButton,
   Slide,
   Dialog,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  DialogContent,
+  DialogActions,
+  SelectChangeEvent,
+  MenuItem,
+  Select,
 } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { AdminUtil, userData } from "../../util/AdminUtil";
@@ -30,6 +38,15 @@ export const AllUsers = (): JSX.Element => {
   const [pages,setPages] = useState<number>(10)
   const [page,setPage]=useState<number>(1)
   const [openCreateDialog,setOpenCreateDialog] = useState<boolean>(false);
+  const [fname,setfname]=useState<string>("")
+  const [lname,setlname] = useState<string>("")
+  const [mail,setmail] = useState<string>("")
+  const [landCode,setLandCode] = useState("+32")
+  const [phnumber,setphnumber] = useState<number>(0)
+  const [pass, setpass] = useState<string>("") 
+  const [vpass,setvpass]=useState<string>("")
+  const [createResult,setCreateResult]=useState<boolean>(false) 
+  const [passwordisValid,setpasswordisValid]=useState<boolean>(false)
   useEffect(() => {
     const controller = new AbortController();
     requestAllUsers(controller)
@@ -54,19 +71,33 @@ const requestAllUsers = (controller:AbortController)=>{
 const HandleResetRequest = (email:string) =>{
   IOUtil.requestPasswordReset(email)
 }
+const CreateUser = ()=>{
+  if(fname.length>0&&lname.length>0&&landCode&&phnumber?.toString().length>0&&pass.length>0&&vpass.length>0){
+  HandleClickCloseCreateDialog();
+  IOUtil.registerUser(fname,lname,mail,landCode+phnumber?.toString(),pass,vpass,(result)=>{
+    setCreateResult(result.succes)
+  })
+  setfname("")
+  setlname("")
+  setphnumber(0)
+  setpass("")
+  setvpass("")
+
+}
+  
+}
 const HandleClickOpenCreateDialog=()=>{
+  console.log("open triggered")
   setOpenCreateDialog(true);
 }
 const HandleClickCloseCreateDialog=()=>{
+  console.log("close triggered")
   setOpenCreateDialog(false);
 }
-const Transition = React.forwardRef(function Transition(
-  props:TransitionProps & {children:React.ReactElement;},
-  ref:React.Ref<unknown>,
-){
-  return <Slide direction="up" ref={ref}{...props}/>
+function passMatch():boolean{
+  if(pass === vpass && pass.length>0)return true;
+  return false;
 }
-)
 
   return (
     <>
@@ -94,11 +125,37 @@ const Transition = React.forwardRef(function Transition(
                 <TableCell>Request Reset</TableCell>
                 <TableCell>{I18n.t("allUsers.tableAdmin")}</TableCell>
                 <TableCell>
-                  <Button endIcon={<AddCircleIcon/>} color="success">
+                  <Button endIcon={<AddCircleIcon/>} color="success" onClick={HandleClickOpenCreateDialog}>
                   {I18n.t("allUsers.tableCreateUser")}
                   </Button>
-                  <Dialog fullScreen TransitionComponent={Transition} onClose={HandleClickCloseCreateDialog} open={openCreateDialog}>
-                      <Button onClick={HandleClickCloseCreateDialog}></Button>
+                  <Dialog onClose={HandleClickCloseCreateDialog} open={openCreateDialog} maxWidth="lg" >
+                    <DialogTitle>Create New User</DialogTitle>
+                    <DialogContent sx={{display:"flex",flexDirection:"inherit", rowGap:".5rem"}}>
+                      <DialogContentText>
+                        Create a new user in the database.<br></br>
+                        Make sure all required fields are filled in.
+                      </DialogContentText>
+                      <TextField label="First name" required onChange={(e)=>setfname(e.target.value)}/>
+                      <TextField label="Last name" required onChange={(e)=>setlname(e.target.value)}/>
+                      <TextField label="email" required onChange={(e)=>setmail(e.target.value)}/>
+                      <Box sx={{display:"inherit",}}>
+                      <Select labelId="Land Code" value={landCode} onChange={(e:SelectChangeEvent)=>{setLandCode(e.target.value as string)}}>
+                        <MenuItem value="+31">+31</MenuItem>
+                        <MenuItem value="+32">+32</MenuItem>
+                        <MenuItem value="+33">+33</MenuItem>
+                        <MenuItem value="+49">+49</MenuItem>
+                      </Select>
+                      <TextField label="Phone Number" required type="number" onChange={(e)=>setphnumber(parseInt(e.target.value))}/>
+                      </Box>
+                      <TextField label="Password" required onChange={(e)=>setpass(e.target.value)}/>
+                      <TextField label="Verify password" required onChange={(e)=>{
+                        setvpass(e.target.value);
+                        setpasswordisValid(passMatch());}}/>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={HandleClickCloseCreateDialog} color="error">Cancel</Button>
+                      <Button onClick={CreateUser} disabled={!passMatch} color="success">Save</Button>
+                    </DialogActions>
                   </Dialog>
 
                 </TableCell>
