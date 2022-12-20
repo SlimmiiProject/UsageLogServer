fd274f34ec0d2bd1f2027ec7bd2f34f1391ac034
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-12-20 12:33:47 +0100

    Fixed

--- client/src/components/admin/AllDevices.tsx
+++ client/src/components/admin/AllDevices.tsx
@@ -1,4 +1,4 @@
-import { AdminUtil, deviceData,userData } from "../../util/AdminUtil";
+import { AdminUtil, deviceData, userData } from "../../util/AdminUtil";
 import { useState, useEffect } from "react";
 import {
   Box,
@@ -20,56 +20,59 @@ import { I18n } from "../../util/language/I18n";
 import { IOUtil } from "../../util/IOUtil";
 
 export interface dialogstate {
-  open:boolean,
-  device:string,
-  user?:number,
-  assign:(deviceid:string,userid:number)=>void,
-  setopen:(deviceid:string)=>void;
+  open: boolean,
+  device: string,
+  user?: number,
+  assign: (deviceid: string, userid: number) => void,
+  setopen: (deviceid: string) => void;
 
 }
 export const AllDevices = (): JSX.Element => {
   const [devices, setDevices] = useState<deviceData[]>([]);
-  const [dialogs,setDialogs] = useState<dialogstate[]>([])
+  const [dialogs, setDialogs] = useState<dialogstate[]>([])
   const [isloading, setisloading] = useState<boolean>(true);
   const [deviceId, setDeviceId] = useState<string>("");
   const [alias, setAlias] = useState<string>("");
 
   useEffect(() => {
     const controller = new AbortController();
-    let temp:dialogstate[] = [];
+    let temp: dialogstate[] = [];
     setisloading(true);
-    AdminUtil.getAllDevices(controller).then((result:deviceData[]) => {
+    AdminUtil.getAllDevices(controller).then((result: deviceData[]) => {
       setDevices(result);
       //populate array with objects for states
-      result.map((r)=>temp.push({
-        open:false,
-        device:r.id,
-        user:r.owner,
-        assign:handleClickClosed,
-        setopen:handleClickOpen}))
+      result.forEach((r) => temp.push({
+        open: false,
+        device: r.id,
+        user: r.owner,
+        assign: handleClickClosed,
+        setopen: handleClickOpen
+      }));
+      
       setDialogs(temp)
-      console.log(temp)
       setisloading(false);
     });
     return () => controller.abort();
   }, []);
-  const handleClickClosed = (deviceid:string,userid:number)=>{
+
+  const handleClickClosed = (deviceid: string, userid: number) => {
     console.log("close triggered")
     console.log(deviceid)
-    console.log(dialogs.filter((d)=>d.device=deviceid))
-    dialogs.filter((d)=>d.device===deviceid).map((di)=>{
+    console.log(dialogs.filter((d) => d.device = deviceid))
+    dialogs.filter((d) => d.device === deviceid).map((di) => {
       console.log(di)
-      di.open=false;})
+      di.open = false;
+    })
     //TODO: assign user to device
   }
-  const handleClickOpen = (deviceid:string)=>{
+  const handleClickOpen = (deviceid: string) => {
     console.log("open triggered")
     let temp = dialogs;
     console.log(dialogs)
     console.log(temp)
-    temp.map((d)=>d.device===deviceid? d.open=true : d.open=false)
+    temp.map((d) => d.device === deviceid ? d.open = true : d.open = false)
     setDialogs(temp)
-  
+
   }
   return (
     <>
@@ -86,7 +89,7 @@ export const AllDevices = (): JSX.Element => {
         }}
       >
         <h2>{I18n.t("allDevices.List")}</h2>
-        <TableContainer component={Paper} arial-label="simple table" sx={{margin:"auto"}}>
+        <TableContainer component={Paper} arial-label="simple table" sx={{ margin: "auto" }}>
           <TableHead>
             <TableRow>
               <TableCell>{I18n.t("allDevices.tableIndex")}</TableCell>
@@ -98,7 +101,7 @@ export const AllDevices = (): JSX.Element => {
           </TableHead>
           <TableBody>
             {!isloading ? (
-              devices.map((device,index) => (
+              devices.map((device, index) => (
                 <TableRow
                   key={device.index}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -114,17 +117,17 @@ export const AllDevices = (): JSX.Element => {
                     {device.owner ? (
                       <p>{device.owner}</p>
                     ) : (<>
-                    <Button onClick={()=>dialogs[index].setopen(device.id)}>
-                      Assign user to device
-                    </Button>
-                    <Dialog open={dialogs[index].open} onClose={()=>dialogs[index].assign(device.id,11)}>
-                      <DialogTitle>Assign user to this device</DialogTitle>
-                      <TextField autoFocus fullWidth margin="dense" label="user id" type="number" variant="standard"/>
-                      <DialogContent>
-                      <Button>temp stuff i place here, users will be displayed here later on</Button>
-                      </DialogContent>
-                    </Dialog>
-                     </>
+                      <Button onClick={() => dialogs[index].setopen(device.id)}>
+                        Assign user to device
+                      </Button>
+                      <Dialog open={dialogs[index].open} onClose={() => dialogs[index].assign(device.id, 11)}>
+                        <DialogTitle>Assign user to this device</DialogTitle>
+                        <TextField autoFocus fullWidth margin="dense" label="user id" type="number" variant="standard" />
+                        <DialogContent>
+                          <Button>temp stuff i place here, users will be displayed here later on</Button>
+                        </DialogContent>
+                      </Dialog>
+                    </>
                     )}
                   </TableCell>
 
@@ -141,46 +144,46 @@ export const AllDevices = (): JSX.Element => {
                     <Chip
                       label={I18n.t("allDevices.tableChipRemove")}
                       variant="outlined"
-                      style={{backgroundColor:'rgba(210,18,25,255)' }}
+                      style={{ backgroundColor: 'rgba(210,18,25,255)' }}
                       onClick={(event) =>
                         IOUtil.deleteDevice(device.id)
                       }
                     />
-                    </TableCell>
+                  </TableCell>
                 </TableRow>
               ))
             ) : (
-              <section className="graph" style={{borderWidth:0,alignItems:"center",justifyItems:"center",justifyContent:"center",display:"flex"}}><CircularProgress className="circularprogress"/></section>
+              <section className="graph" style={{ borderWidth: 0, alignItems: "center", justifyItems: "center", justifyContent: "center", display: "flex" }}><CircularProgress className="circularprogress" /></section>
             )}
             <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                <TableCell align="left" >
-                    <label>{I18n.t("allDevices.tableId")}</label>
-                </TableCell>
-                <TableCell align="left">
-                    <input type="text" value={deviceId} onChange={(e) => setDeviceId(e.target.value)} />
-                </TableCell>
-                <TableCell align="left">
-                    <label>{I18n.t("allDevices.tableAlias")}</label>
-                </TableCell>
-                <TableCell align="left">
-                    <input type="text" value={alias} onChange={(e) => setAlias(e.target.value)} />
-                </TableCell>
-                <TableCell align="center">
+              <TableCell align="left" >
+                <label>{I18n.t("allDevices.tableId")}</label>
+              </TableCell>
+              <TableCell align="left">
+                <input type="text" value={deviceId} onChange={(e) => setDeviceId(e.target.value)} />
+              </TableCell>
+              <TableCell align="left">
+                <label>{I18n.t("allDevices.tableAlias")}</label>
+              </TableCell>
+              <TableCell align="left">
+                <input type="text" value={alias} onChange={(e) => setAlias(e.target.value)} />
+              </TableCell>
+              <TableCell align="center">
 
                 <Chip
-                      label={I18n.t("allDevices.tableChipAdd")}
-                      variant="outlined"
-                      style={{backgroundColor:'rgba(25, 118, 210, 255)' }}
-                      onClick={(event) => {
-                        IOUtil.addDevice(deviceId, alias)
-                    }
-                    }
-                    />
-                </TableCell>
+                  label={I18n.t("allDevices.tableChipAdd")}
+                  variant="outlined"
+                  style={{ backgroundColor: 'rgba(25, 118, 210, 255)' }}
+                  onClick={(event) => {
+                    IOUtil.addDevice(deviceId, alias)
+                  }
+                  }
+                />
+              </TableCell>
             </TableRow>
           </TableBody>
         </TableContainer>
-        <Box className="marginFix" sx={{minHeight:"2rem"}}/>
+        <Box className="marginFix" sx={{ minHeight: "2rem" }} />
 
       </Box>
     </>
