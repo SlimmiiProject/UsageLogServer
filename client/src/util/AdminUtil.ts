58d0b72900199671156f957608470de6ade4bb70
Raven Van Hove <raven.vanhove@student.ap.be>
2022-12-20 14:51:03 +0100

    added function

--- client/src/util/AdminUtil.ts
+++ client/src/util/AdminUtil.ts
@@ -60,6 +60,23 @@ export class AdminUtil {
         }
     }
 
+    public static getUser = async (userId: number) : Promise<userData> => {
+        try {
+            const res = await IOUtil.INSTANCE.get(`/admin/user`, {params: {userId: userId}});
+            return res.data;
+        } catch (_ignore) {
+            return {
+                userId: 0,
+                firstname: "",
+                lastname: "",
+                email: "",
+                device: [],
+                phone: "",
+                isAdmin: false
+            }
+        }
+    }
+
     /* A function that is called when a user is created. */
     public static getUsers = async (controller: AbortController, page: number): Promise<userResponseData> => {
         const toSkip = page * 10;
