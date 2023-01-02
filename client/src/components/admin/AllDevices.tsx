371a2ab711ada1a2d91fe1c8512b9e5763dcf5ef
Elwyn Van der Borght <38124619+ElwynVdb@users.noreply.github.com>
2023-01-02 17:26:49 +0100

    Core functions (#58)
    * I blame Alex
    
    * Fix Attempt
    
    * Password reset
    
    * Password Reset
    
    * Reload Fix
    
    * Link Fix
    
    * Fixed I hope
    
    * Validate
    
    * Fix
    
    * Fixes
    
    * Detail
    
    * Fixed UrlSafe
    
    * Fix
    
    * Timeout fix
    
    * Admin Page Style
    
    * Edit profile
    
    * Fixed double hashing
    
    * Fix
    
    * Cleaner
    
    * Fix Security Breach
    
    * Small Fixes
    
    * TODO
    
    * -
    
    * Raw login response

--- client/src/components/admin/AllDevices.tsx
+++ client/src/components/admin/AllDevices.tsx
@@ -11,10 +11,6 @@ import {
   Paper,
   CircularProgress,
   Button,
-  Dialog,
-  DialogTitle,
-  TextField,
-  DialogContent,
   Pagination,
   Stack,
 } from "@mui/material";
@@ -50,9 +46,12 @@ export const AllDevices = (): JSX.Element => {
     console.log("close triggered")
     console.log(deviceid)
     console.log(dialogs.filter((d) => d.device = deviceid))
+    
+    // TODO This doesn't do anything, state isn't updated
     dialogs.filter((d) => d.device === deviceid).map((di) => {
       console.log(di)
       di.open = false;
+      return di;
     })
     //TODO: assign user to device
   }
