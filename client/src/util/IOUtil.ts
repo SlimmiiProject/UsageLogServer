d121c9235201a0c936915eda8a1bf08e4882ee88
Raven Van Hove <raven.vanhove@student.ap.be>
2023-01-05 13:22:11 +0100

    oops vergeten

--- client/src/util/IOUtil.ts
+++ client/src/util/IOUtil.ts
@@ -217,11 +217,11 @@ export class IOUtil {
   public static changeDeviceAlias = async (deviceIndex: number, alias: string, controller: AbortController) => {
     try {
       console.log(deviceIndex, alias)
-      const res = await this.INSTANCE.post("/profiles/device-alias", {
+      const res = await this.INSTANCE.put("/profiles/device-alias", {
           deviceIndex: deviceIndex,
           alias: alias
       });
-      return res.data;
+      return res.status === 200;
     } catch (err) {
       console.error(err)
       return false;
