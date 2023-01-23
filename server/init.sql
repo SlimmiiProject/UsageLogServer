c8ee3bd082862e72e122b2a9da05515e76b7f77b
Van Dijck Alex (s131627) <alex.vandijck@student.ap.be>
2023-01-23 08:48:54 +0100

    Added init sql

---
+++ server/init.sql
@@ -0,0 +1,8 @@
+-- --------------------------------------------------------
+-- Host:                         local
+-- Server versie:                10.3.35-MariaDB - MariaDB Server
+-- Server OS:                    Linux
+-- HeidiSQL Versie:              12.3.0.6589
+-- --------------------------------------------------------
+
+CREATE DATABASE IF NOT EXISTS `itcasetest`;
